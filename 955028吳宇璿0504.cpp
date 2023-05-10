d7e0a7ba2184d431bad6a2a795e38063846c7300
arnold060708 <0607arnold@gmail.com>
2023-05-10 21:51:37 +0800

    955028吳宇璿0504作業

---
+++ 955028吳宇璿0504.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (!(n == 0)) {
+        queue<int> cards;
+
+        for (int i = 1; i <= n; i++) {
+            cards.push(i);
+        }
+
+        cout << "Discarded cards: ";
+
+        while (cards.size() > 1) {
+            cout << cards.front();
+            if (cards.size() == 2) {
+                cout << "";
+            } else {
+                cout << ", ";
+            }
+
+            cards.pop();
+            cards.push(cards.front());
+            cards.pop();
+        }
+
+        cout << endl << "Remaining card: " << cards.front() << endl;
+        cin >> n;
+    }
+}
